2abc14047c6fa062493b9fa0d0e8fcd7fa3918ca
Julius Mercene <juliusmercene@yahoo.com>
2022-11-23 20:58:13 -0800

    addded the operator functions to work with the buttons as well as the equals button to run the compute function. The biggest problem I had so far with this commit was that I had trouble adding the second integer because I did not get the resetScreen function to work which caused new numbers to not be inputed after the operation button was called and would just add the second integer to the first integer's string. i fixed this by adding shouldResetScreen to the compute function, appendNumber function as well as added its complimentary resetScreen function. The problem I have now that I have to fix before moving on through the next steps with TOP is to fix the problem that only restricts the input numbers to only be single digits. Even after clicking the same number or after using the operator buttons, I could only input single digit numbers. I would also like to add that I solved my main problem in this commit mostly by using dev tools and pin pointing where the code would possibly break when clicking the numbers button... and console logging. I plan to use this method again to solve the next problem regarding only being able to input single digit numbers.

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 let firstOperand = 0;
 let secondOperand = 0;
 let currentOperation = null;
+let shouldResetScreen = false;
 
 
 const add = function() { // add every number in the array
@@ -60,6 +61,42 @@ const divide = function(x, y) { // divide x by y
   const lastOperationScreen = document.getElementById("lastOperation");
   const currentOperationScreen = document.getElementById("currentOperation");
   const numberButtons = document.querySelectorAll('[data-number]');
+  const operationButtons = document.querySelectorAll('[data-operator]');
+  const equalsButton = document.getElementById("equals");
+
+  equalsButton.addEventListener("click", compute);
+  
+  function chooseOperation(operation){
+    if (currentOperation !== null) {
+      compute();
+    }
+    currentOperation = operation;
+    firstOperand = parseFloat(currentOperationScreen.innerText);
+    lastOperationScreen.innerText = `${firstOperand} ${currentOperation}`;
+    shouldResetScreen = true;
+  }
+
+  function compute(){
+    if (currentOperation === null || shouldResetScreen) return;
+    if (currentOperationScreen.innerText === "0" && currentOperation === "/"){
+      alert("You can't divide by 0");
+      return;
+    }
+    secondOperand = parseFloat(currentOperationScreen.innerText);
+    currentOperationScreen.innerText = roundNumber(operate(currentOperation, firstOperand, secondOperand));
+    lastOperationScreen.innerText = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;
+  }
+
+  function roundNumber(number){
+    return Math.round(number * 1000) / 1000;
+  }
+
+  operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      chooseOperation(button.innerText);
+    })
+  })
 
 
 clearButton.addEventListener("click", clear);
@@ -69,12 +106,15 @@ clearButton.addEventListener("click", clear);
 );
 
 function appendNumber(number){
-  if (currentOperationScreen.textContent === "0"){
-    currentOperationScreen.textContent = number;
-  }
-  else{ 
+  if (currentOperationScreen.textContent === "0" || shouldResetScreen) {
+    resetScreen();
     currentOperationScreen.textContent += number;
   }
+}
+
+function resetScreen(){
+  currentOperationScreen.textContent = "";
+  shouldResetScreen = false;
 }
 
 
@@ -85,7 +125,7 @@ function clear(){
     currentOperationScreen.textContent = "0";
     firstOperand = '';
     secondOperand = '';
-    //currentOperation = null;
+    currentOperation = null;
   };
    
   //git test
