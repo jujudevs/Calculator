25416897b611451e4ba9f646915f7acd3a199dbb
Julius Mercene <juliusmercene@yahoo.com>
2022-11-28 14:19:44 -0800

    Added both decimal and backspace functions. The decimal function only allows the decimal button to work once if there is already one in the display. The backspace function allows the user to undo if they click the wrong number.

--- calculator.js
+++ calculator.js
@@ -63,19 +63,26 @@ const divide = function(x, y) {
   const numberButtons = document.querySelectorAll('[data-number]');
   const operationButtons = document.querySelectorAll('[data-operator]');
   const equalsButton = document.getElementById("equals");
+  const decimalButton = document.getElementById("decimal");
+  const deleteButton = document.getElementById("backspace");
 
-  equalsButton.addEventListener("click", compute);
   
-  function chooseOperation(operation){
-    if (currentOperation !== null) {
-      compute();
-    }
-    currentOperation = operation;
-    firstOperand = parseFloat(currentOperationScreen.innerText);
-    lastOperationScreen.innerText = `${firstOperand} ${currentOperation}`;
-    shouldResetScreen = true;
+  deleteButton.addEventListener("click", deleteNumber);
+  function deleteNumber(){
+    currentOperationScreen.textContent = currentOperationScreen.textContent.toString().slice(0, -1);
   }
 
+  decimalButton.addEventListener("click", addDecimal);
+  function addDecimal(){
+    if (currentOperationScreen.textContent.includes(".")){
+      return;
+    }
+    currentOperationScreen.textContent += ".";
+  }
+
+ 
+
+  equalsButton.addEventListener("click", compute);
   function compute(){
     if (currentOperation === null || shouldResetScreen) return;
     if (currentOperationScreen.innerText === "0" && currentOperation === "/"){
@@ -88,18 +95,37 @@ const divide = function(x, y) {
     currentOperation = null;
   }
 
-  function roundNumber(number){
-    return Math.round(number * 1000) / 1000;
-  }
-
-  operationButtons.forEach(button => {
+operationButtons.forEach(button => {
     button.addEventListener("click", () => {
       chooseOperation(button.innerText);
     })
   })
+  function chooseOperation(operation){
+    if (currentOperation !== null) {
+      compute();
+    }
+    currentOperation = operation;
+    firstOperand = parseFloat(currentOperationScreen.innerText);
+    lastOperationScreen.innerText = `${firstOperand} ${currentOperation}`;
+    shouldResetScreen = true;
+  }
+
+  
+  function roundNumber(number){
+    return Math.round(number * 1000) / 1000;
+  }
+
+  
 
 
 clearButton.addEventListener("click", clear);
+function clear(){
+    lastOperationScreen.textContent = "";
+    currentOperationScreen.textContent = "0";
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+  };
 
  numberButtons.forEach((button) => 
   button.addEventListener("click", () => appendNumber(button.textContent))
@@ -119,12 +145,5 @@ function resetScreen(){
 
 
 
-function clear(){
-    lastOperationScreen.textContent = "";
-    currentOperationScreen.textContent = "0";
-    firstOperand = '';
-    secondOperand = '';
-    currentOperation = null;
-  };
    
   
